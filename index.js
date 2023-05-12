a54fe8a3a5409939570351533c2a823e3d99d671
J Morbos <morbosjessiejohn@gmail.com>
2023-05-12 08:17:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.listen(port, () => console.log(`Server started on ${port}`));
 const inventoryMsg = `<b><u>TOTAL OF NECESSARY OFFICE ITEMS</u></b>\n<b>System Unit:</b> 11 \n<b>Work phone: </b>14 iPhone
 `;
 
-let inventoryList = `<b>Date: </b> ${updatedDate} \n<b>Items: </b>\n 1pc item_name\n 1pc item_name\n\n<b>Deploy: </b> (optional) \n`;
+let inventoryList = `<b>Date: </b> ${updatedDate} \n<b>Items: </b>\n 1pc item_name\n 1pc item_name\n\n<b>Deploy: </b> (Computer No.) \n`;
 
 
 
